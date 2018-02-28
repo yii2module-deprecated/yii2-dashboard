af4d9d60862a717817396afb5872e7544247aac1
Vitaliy <theyamshikov@yandex.ru>
2018-02-28 12:50:51 +0600

    make domain class and route config

---
+++ src/api/config/routes.php
@@ -0,0 +1,7 @@
+<?php
+
+$version = API_VERSION_STRING;
+
+return [
+	"GET {$version}/doc" => "dashboard/default/index",
+];
---
+++ src/domain/Domain.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace yii2module\dashboard\domain;
+
+class Domain extends \yii2lab\domain\Domain {
+	
+	public function config() {
+
+		return [
+			'repositories' => [
+				'article',
+			],
+			'services' => [
+				'article',
+			],
+		];
+	}
+	
+}
