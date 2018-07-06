46dee2d77c4aad9e2f7ea9a7e0b84d4f1cfbebef
Vitaliy <theyamshikov@yandex.ru>
2018-07-06 22:17:31 +0600

    api doc link

--- src/api/controllers/DefaultController.php
+++ src/api/controllers/DefaultController.php
@@ -4,16 +4,19 @@ namespace yii2module\dashboard\api\controllers;
 
 use Yii;
 use yii\rest\Controller;
+use yii2lab\rest\domain\helpers\MiscHelper;
 
 class DefaultController extends Controller
 {
 	
 	public function actionIndex() {
-	    return [
-	        'title' => Yii::t('dashboard/main', 'title'),
-            'header' => Yii::t('dashboard/main', 'hello'),
-            'text' => Yii::t('dashboard/main', 'text'),
-        ];
+	    $data = [
+		    'title' => Yii::t('dashboard/main', 'title'),
+		    'header' => Yii::t('dashboard/main', 'hello'),
+		    'text' => Yii::t('dashboard/main', 'text'),
+	    ];
+		$data['text'] .= PHP_EOL . 'Для просмотра документации API переидите по ссылке: ' . MiscHelper::forgeApiUrl('doc/html');
+		return $data;
     }
 
 }
