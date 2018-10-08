08317f9869ee68681df10c00b789ef29588eb723
Vitaliy <theyamshikov@yandex.ru>
2018-10-08 17:47:00 +0600

    clean

--- src/web/controllers/DefaultController.php
+++ src/web/controllers/DefaultController.php
@@ -10,16 +10,6 @@ class DefaultController extends Controller
 	
 	public function actionIndex()
 	{
-		/*$query = Query::forge();
-		$query->page(2);
-		$query->limit(10);
-		//$query->perPage(10);l
-		//$dataProvider = \App::$domain->history->transaction->getDataProvider($query);
-		//prr($dataProvider->getPagination()->offset,1,1);
-		
-		$collection = \App::$domain->history->transaction->oneById(50032158);
-		prr($collection,1,1);*/
-		
 		return $this->render('index');
 	}
 
