7295ae79e45ab777225d7e92893b2390a5f562e2
Vitaliy <theyamshikov@yandex.ru>
2018-10-08 17:46:23 +0600

    clean

--- src/admin/Module.php
+++ src/admin/Module.php
@@ -2,7 +2,7 @@
 
 namespace yii2module\dashboard\admin;
 use yii2lab\applicationTemplate\common\enums\ApplicationPermissionEnum;
-use yii2lab\helpers\Behavior;
+use yii2lab\extension\web\helpers\Behavior;
 
 /**
  * Class Module
--- src/web/controllers/DefaultController.php
+++ src/web/controllers/DefaultController.php
@@ -3,12 +3,23 @@
 namespace yii2module\dashboard\web\controllers;
 
 use yii\web\Controller;
+use yii2lab\domain\data\Query;
 
 class DefaultController extends Controller
 {
 	
 	public function actionIndex()
 	{
+		/*$query = Query::forge();
+		$query->page(2);
+		$query->limit(10);
+		//$query->perPage(10);l
+		//$dataProvider = \App::$domain->history->transaction->getDataProvider($query);
+		//prr($dataProvider->getPagination()->offset,1,1);
+		
+		$collection = \App::$domain->history->transaction->oneById(50032158);
+		prr($collection,1,1);*/
+		
 		return $this->render('index');
 	}
 
